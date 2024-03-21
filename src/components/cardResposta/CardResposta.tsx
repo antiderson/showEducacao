7340780bc439af6aeb1cb8d7f6c841097bb60dbb
Anderson Daniel <anderbreiksondaniel@gmail.com>
2024-03-21 18:53:13 -0300

    tentativa rd

--- src/components/cardResposta/CardResposta.tsx
+++ src/components/cardResposta/CardResposta.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import styles from './index.module.css'
+import styles from './index.module.css';
 
 interface CardRespostaProps {
     content: string;
@@ -7,7 +7,7 @@ interface CardRespostaProps {
     onClick: () => void;
 }
 
-const CardResposta: React.FC<CardRespostaProps> = ({ content, onClick }) => {
+const CardResposta: React.FC<CardRespostaProps> = ({ content, correta, onClick }) => {
     const [selecionado, setSelecionado] = useState<boolean>(false);
 
     const handleClick = () => {
@@ -17,8 +17,12 @@ const CardResposta: React.FC<CardRespostaProps> = ({ content, onClick }) => {
         }
     };
 
+    // Classes dinâmicas com base nas propriedades correta e selecionado
+    const respostaClasses = `${styles.cardResp} ${selecionado ? (correta ? styles.correta : styles.errada) : ''
+        }`;
+
     return (
-        <button className={styles.cardResp} onClick={handleClick}>
+        <button className={respostaClasses} onClick={handleClick}>
             <h2 className={styles.textResp}>{content}</h2>
         </button>
     );
